3c30ac22d77ae7d5ecdce39f2948e7a27fdcb263
Farid Tri Wicaksono <farid.wicak@gmail.com>
2025-12-01 12:34:26 +0700

    fix: missing config mysql

--- src/infra/index.js
+++ src/infra/index.js
@@ -37,6 +37,7 @@ const config = {
    mysqlConfig: {
       connectionLimit: process.env.MYSQL_CONNECTION_LIMIT,
       host: process.env.MYSQL_HOST,
+      port: process.env.MYSQL_POST,
       user: process.env.MYSQL_USER,
       password: process.env.MYSQL_PASSWORD,
       database: process.env.MYSQL_DATABASE
